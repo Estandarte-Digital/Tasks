5544519c4042a8a805d089c59c95cc0b258edb2e
Francisco Gonzalez Hernandez <franciscogonzalez_hernandez@hotmail.com>
2020-09-17 16:04:10 -0700

    Se cambian errores en tasks

--- automatization.bat
+++ automatization.bat
@@ -12,7 +12,7 @@ schtasks /create /tn Pontelo /tr C:\Users\%USER%\PycharmProjects\Tasks\pontelo.b
 schtasks /create /tn ClimaInfo /tr C:\Users\%USER%\PycharmProjects\Tasks\climainfor.bat /sc onlogon /rl HIGHEST
 schtasks /create /tn EmotionRecognition /tr C:\Users\%USER%\PycharmProjects\Tasks\emotion_recognition0.bat /sc onlogon /rl HIGHEST
 schtasks /create /tn Mask_Detector /tr C:\Users\%USER%\PycharmProjects\Tasks\mask_detector.bat /sc onlogon /rl HIGHEST
-schtasks /create /tn Unity /tr C:\Users\%USER%\PycharmProjects\Tasks\mask_detector.bat /sc onlogon /rl HIGHEST
+schtasks /create /tn Unity /tr "C:\Users\%USER%\Documents\EDBuild03\EstandarteDigitalP - Shortcut.lnk" /sc onlogon /rl HIGHEST
 
 cd C:\Users\%USER%\PycharmProjects\ClimaInfo
 npm install
