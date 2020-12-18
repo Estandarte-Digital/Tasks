65fccaadd42ffef98067759c675132b49aa09159
Francisco Gonzalez Hernandez <franciscogonzalez_hernandez@hotmail.com>
2020-12-17 17:45:02 -0700

    Cambios para nuevo estandarte

---
+++ fusion.bat
@@ -0,0 +1,4 @@
+call conda activate estandarte
+cd C:\Users\%username%\PycharmProjects\fusion_recognizers\
+python C:\Users\%username%\PycharmProjects\fusion_recognizers\fusion.py
+pause
--- install_software.ps1
+++ install_software.ps1
@@ -2,7 +2,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n=allowGlobalConfirmation
 choco install git /GitOnlyOnPath
-choco install nodejs
 choco install miniconda3 --version=4.7.12.1
 refreshenv
 cmd.exe /c "automatization.bat"
