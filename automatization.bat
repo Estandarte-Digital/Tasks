9f436b9d6e7d7016e7da612b623e538942032df1
EstandarteDigital01 <rodolfoferroperez@gmail.com>
2021-01-05 19:09:09 -0600

    Cambios para automatizacion

--- automatization.bat
+++ automatization.bat
@@ -1,23 +1,25 @@
-set USER=%username%
+mkdir C:\Users\%username%\PycharmProjects\
+mkdir C:\Users\%username%\outputs\
+mkdir C:\Users\%username%\outputs\logs\
+cd C:\Users\%username%\PycharmProjects\
 
-mkdir C:\Users\%USER%\PycharmProjects\
-mkdir C:\Users\%USER%\outputs\
-cd C:\Users\%USER%\PycharmProjects\
-
-git clone https://github.com/Estandarte-Digital/Tasks
 git clone https://github.com/Estandarte-Digital/fusion_recognizers
+git clone https://github.com/Estandarte-Digital/status_bot.git
+git clone https://github.com/Estandarte-Digital/emotions_report.git
 
-cd C:\Users\%USER%\PycharmProjects\fusion_recognizers
+cd C:\Users\%username%\PycharmProjects\fusion_recognizers
 git submodule init
 git submodule update
 
-cd C:\Users\%USER%\PycharmProjects
 
-schtasks /create /tn fusion /tr C:\Users\%USER%\PycharmProjects\Tasks\fusion.bat /sc daily /st 09:00 /rl HIGHEST
-schtasks /create /tn taskkill /tr C:\Users\%USER%\PycharmProjects\Tasks\kill_processes.bat /sc daily /st 21:00 /rl HIGHEST
-schtasks /create /tn Unity /tr "C:\Users\%USER%\Documents\EDBuild03\EstandarteDigitalP - Shortcut.lnk" /sc daily /st 09:00 /rl HIGHEST
+schtasks /create /tn fusion /tr C:\Users\%username%\PycharmProjects\Tasks\fusion.bat /sc daily /st 08:55 /rl HIGHEST
+schtasks /create /tn Unity /tr "C:\Users\%username%\PycharmProjects\EstandarteDigitalP\EstandarteDigitalP.exe" /sc daily /st 09:00 /rl HIGHEST
+schtasks /create /tn bot /tr C:\Users\%username%\PycharmProjects\Tasks\status_bot.bat /sc daily /st 09:00 /rl HIGHEST
+schtasks /create /tn taskkill /tr C:\Users\%username%\PycharmProjects\Tasks\kill_processes.bat /sc daily /st 21:00 /rl HIGHEST
+schtasks /create /tn emotion_report /tr C:\Users\%username%\PycharmProjects\Tasks\emotions_report.bat /sc daily /st 21:05 /rl HIGHEST
 
-cd Tasks
+
+cd C:\Users\%username%\PycharmProjects\Tasks
 conda env create -f environment.yml
 call conda estandarte
 pip install -r requirements.txt
