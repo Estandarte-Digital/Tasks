92162e2fe81e7fe0e3010a014462de10596e6f58
Francisco Gonzalez Hernandez <franciscogonzalez_hernandez@hotmail.com>
2021-03-16 10:32:30 -0700

    Nuevos archivos para automatizacion

--- automatization.bat
+++ automatization.bat
@@ -12,16 +12,6 @@ git submodule init
 git submodule update
 
 
-schtasks /create /tn fusion /tr C:\Users\%username%\PycharmProjects\Tasks\fusion.bat /sc daily /st 08:55 /rl HIGHEST
-schtasks /create /tn Unity /tr "C:\Users\%username%\PycharmProjects\EstandarteDigitalP\EstandarteDigitalP.exe" /sc daily /st 09:00 /rl HIGHEST
-schtasks /create /tn bot /tr C:\Users\%username%\PycharmProjects\Tasks\status_bot.bat /sc daily /st 09:00 /rl HIGHEST
-schtasks /create /tn taskkill /tr C:\Users\%username%\PycharmProjects\Tasks\kill_processes.bat /sc daily /st 21:00 /rl HIGHEST
-schtasks /create /tn emotion_report /tr C:\Users\%username%\PycharmProjects\Tasks\emotions_report.bat /sc daily /st 21:05 /rl HIGHEST
-schtasks /create /tn fusion_encendido /tr C:\Users\%username%\PycharmProjects\Tasks\fusion.bat /sc onlogon /rl HIGHEST
-schtasks /create /tn Unity_encendido /tr "C:\Users\%username%\PycharmProjects\EstandarteDigitalP\EstandarteDigitalP.exe" /sc onlogon /rl HIGHEST
-schtasks /create /tn bot_encendido /tr C:\Users\%username%\PycharmProjects\Tasks\status_bot.bat /sc onlogon /rl HIGHEST
-
-
 cd C:\Users\%username%\PycharmProjects\Tasks
 conda env create -f environment.yml
 call conda estandarte
