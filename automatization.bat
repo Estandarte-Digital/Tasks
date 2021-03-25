20e4bc227c5305a113b1e1d68b3a4b7baa44ae24
Francisco Gonzalez Hernandez <franciscogonzalez_hernandez@hotmail.com>
2021-03-25 17:57:37 -0600

    Avance para automatizacion, faltan tareas

--- automatization.bat
+++ automatization.bat
@@ -2,8 +2,18 @@ mkdir C:\Users\%username%\PycharmProjects\
 mkdir C:\Users\%username%\PycharmProjects\EstandarteDigitalP\
 mkdir C:\Users\%username%\outputs\
 mkdir C:\Users\%username%\outputs\logs\
-cd C:\Users\%username%\PycharmProjects\
 
+git config --global user.name "Francisco González Hernández"
+git config --global user.email "franciscogonzalez_hernandez@hotmail.com"
+git config credential.helper store
+git push https://github.com/Estandarte-Digital/Tasks.git
+
+cd C:\Users\%username%\PycharmProjects\Tasks
+git init
+git remote add origin https://github.com/Estandarte-Digital/Tasks.git
+git pull origin master --allow-unrelated-histories
+
+cd C:\Users\%username%\PycharmProjects\
 git clone https://github.com/Estandarte-Digital/fusion_recognizers
 git clone https://github.com/Estandarte-Digital/status_bot.git
 git clone https://github.com/Estandarte-Digital/emotions_report.git
@@ -12,7 +22,8 @@ cd C:\Users\%username%\PycharmProjects\fusion_recognizers
 git submodule init
 git submodule update
 
-
+cd C:\Users\%username%\PycharmProjects\Tasks\programacion_tareas
+python adapt_xml.py
 
 
 cd C:\Users\%username%\PycharmProjects\Tasks
