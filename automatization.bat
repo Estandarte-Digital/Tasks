b250b39f48501df230032ab6ede0b16a0e03f421
Francisco Gonzalez Hernandez <franciscogonzalez_hernandez@hotmail.com>
2021-03-26 17:01:06 -0600

    Cambios para automatizar libreria de impresión de PDF con estadistica

--- automatization.bat
+++ automatization.bat
@@ -3,15 +3,17 @@ mkdir C:\Users\%username%\PycharmProjects\EstandarteDigitalP\
 mkdir C:\Users\%username%\outputs\
 mkdir C:\Users\%username%\outputs\logs\
 
+cd C:\Users\%username%\PycharmProjects\Tasks
 git config --global user.name "Francisco González Hernández"
 git config --global user.email "franciscogonzalez_hernandez@hotmail.com"
 git config credential.helper store
-git push https://github.com/Estandarte-Digital/Tasks.git
 
-cd C:\Users\%username%\PycharmProjects\Tasks
 git init
 git remote add origin https://github.com/Estandarte-Digital/Tasks.git
 git pull origin master --allow-unrelated-histories
+git lfs install
+
+start /wait gtk3-runtime-3.24.24-2021-01-30-ts-win64.exe
 
 cd C:\Users\%username%\PycharmProjects\
 git clone https://github.com/Estandarte-Digital/fusion_recognizers
